d2bc2133d365cef7178212821120fa713611b03c
benfdking <benfdking@users.noreply.github.com>
2020-06-05 20:22:03 +0100

    Fixed an error

--- pkg/defaultjwt/defaultjwt.go
+++ pkg/defaultjwt/defaultjwt.go
@@ -17,13 +17,12 @@ func Default(key crypto.PrivateKey) (string, error) {
 	token.Set(jwt.SubjectKey, defaultSubject)
 	token.Set(jwt.AudienceKey, "AuthenticationGurus")
 	token.Set(jwt.IssuerKey, "https://github.com/benfking/jen ")
-	token.Set(jwt.ExpirationKey, time.Now().Add(1 * time.Hour).Unix())
+	token.Set(jwt.ExpirationKey, time.Now().Add(1*time.Hour).Unix())
 	token.Set(jwt.NotBeforeKey, time.Now())
 	token.Set(jwt.IssuedAtKey, time.Now())
 	output, err := token.Sign(jwa.RS256, key)
 	if err != nil {
-		return "", fmt.Errorf("failed to generate signed payload: %w\n", err)
+		return "", fmt.Errorf("failed to generate signed payload: %w", err)
 	}
 	return string(output), nil
 }
-
